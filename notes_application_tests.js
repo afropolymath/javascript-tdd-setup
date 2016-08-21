4f5e89076269c102db43601f69bf733c9bc23682
robocopkaka <robocopkaka@gmail.com>
2016-08-21 23:13:44 +0100

    Added tests for list_notes and search

--- notes_application_tests.js
+++ notes_application_tests.js
@@ -6,39 +6,39 @@ var assert = chai.assert;
 var NotesApplication = require('./lib/notesapplication.js');
 var Notes = require('./lib/notes.js');
 
-describe("Check that creating a note object properly", function(){
+describe("Check that creating a note object works properly", function(){
 	it("should assign the note content based on the parameter supplied",function(){
-		var an_object = {
+		var person = {
 			author: "kachi",
 			note_content: "stuff"
 		};
-		var note = new Notes(an_object);
+		var note = new Notes(person);
 		assert(note.note_content == "stuff");
 	})
 
 	it("should assign the author based on the parameter supplied",function(){
-		var an_object = {
+		var person = {
 			author: "kachi",
 			note_content: "stuff"
 		};
-		var note = new Notes(an_object);
+		var note = new Notes(person);
 		assert(note.author == "kachi");
 	})
 })
 
-describe("Shoud check that you can create a new note", function(){
+describe("should test that create note function works", function(){
 	it("it should check that a new entry was added", function(){
 		var note = new Notes({author: "kachi", note_content: "stuff"});
-		var check = new NotesApplication();
-		assert(check.notes.length === 0);
-		check.create_notes(note);
-		assert(check.notes.length === 1);
+		var noteapp_object = new NotesApplication();
+		assert(noteapp_object.notes.length === 0);
+		noteapp_object.create_notes(note);
+		assert(noteapp_object.notes.length === 1);
 	})
 
 	it("it should check if the parameter being supplied is an instance of the Notes class", function(){
 		var note = new Notes({author: "kachi", note_content: "stuff"});
-		var check = new NotesApplication();
-		check.create_notes(note);
+		var noteapp_object = new NotesApplication();
+		noteapp_object.create_notes(note);
 		assert.instanceOf(note, Notes);
 	})
 })
@@ -46,17 +46,17 @@ describe("Shoud check that you can create a new note", function(){
 describe("it should check if you can get a note", function(){
 	it("check if you can retrieve a note with an ID of 0", function(){
 		var note = new Notes({author: "kachi", note_content: "stuff"});
-		var check = new NotesApplication();
-		check.create_notes(note);
-		check.get(0);
-		assert(check.notes[0].note_content === "stuff");
+		var noteapp_object = new NotesApplication();
+		noteapp_object.create_notes(note);
+		noteapp_object.get(0);
+		assert(noteapp_object.notes[0].note_content === "stuff");
 	})
 
-	it("check that you can't retrieve a note with an invalid ID", function(){
+	it("throws error when trying to get a note with an invalid id", function(){
 		var note = new Notes({author: "kachi", note_content: "stuff"});
-		var check = new NotesApplication();
-		check.create_notes(note);
-		check.get(4.5);
+		var noteapp_object = new NotesApplication();
+		noteapp_object.create_notes(note);
+		noteapp_object.get(4.5);
 		assert("ID is invalid");
 	})
 
@@ -65,18 +65,18 @@ describe("it should check if you can get a note", function(){
 describe("edit functionality works properly", function(){
 	it("it should check that the content of a note at index 0 changes when edited", function(){
 		var note = new Notes({author: "kachi", note_content: "stuff"});
-		var check = new NotesApplication();
+		var noteapp_object = new NotesApplication();
 
-		check.create_notes(note);
-		check.edit(0, "new stuff")
-		assert(check.notes[0].note_content === "new stuff");
+		noteapp_object.create_notes(note);
+		noteapp_object.edit(0, "new stuff")
+		assert(noteapp_object.notes[0].note_content === "new stuff");
 	})
 
-	it("check that you can't edit a note with an invalid ID", function(){
+	it("throws an error when you try to edit a note with an invalid ID", function(){
 		var note = new Notes({author: "kachi", note_content: "stuff"});
-		var check = new NotesApplication();
-		check.create_notes(note);
-		check.edit(0.1, "new stuff")
+		var noteapp_object = new NotesApplication();
+		noteapp_object.create_notes(note);
+		noteapp_object.edit(0.1, "new stuff")
 		assert("ID is invalid")
 	})
 })
@@ -85,22 +85,46 @@ describe("that you can delete a note", function(){
 	it("check that you can delete a note at index 1 ", function(){
 		var note = new Notes({author: "kachi", note_content: "stuff"});
 		var note2 = new Notes({author: "kachi", note_content: "stuff2"});
-		var check = new NotesApplication();
-		check.create_notes(note);
-		check.create_notes(note2);
-		assert(check.notes.length === 2);
-		check.delete(1);
-		assert(check.notes.length == 1);
+		var noteapp_object = new NotesApplication();
+		noteapp_object.create_notes(note);
+		noteapp_object.create_notes(note2);
+		assert(noteapp_object.notes.length === 2);
+		noteapp_object.delete(1);
+		assert(noteapp_object.notes.length == 1);
 
 	})
 
-	it("checks that you can't delete a note at an invalid index", function(){
+	it("throws an error when you try to delete a note at an invalid index", function(){
 		var note = new Notes({author: "kachi", note_content: "stuff"});
 		var note2 = new Notes({author: "kachi", note_content: "stuff2"});
-		var check = new NotesApplication();
-		check.create_notes(note);
-		check.create_notes(note2);
-		check.delete(5.1);
+		var noteapp_object = new NotesApplication();
+		noteapp_object.create_notes(note);
+		noteapp_object.create_notes(note2);
+		noteapp_object.delete(5.1);
 		assert("ID is invalid")
+	})
+})
+
+describe("Check that the user can retrieve notes", function(){
+	it("should check that the user can retrieve added notes", function(){
+		var note = new Notes({author: "kachi", note_content: "stuff"});
+		var note1 = new Notes({author: "me", note_content: "more stuff"});
+		var note2 = new Notes({author: "me", note_content: "more stuff"});
+		var noteapp_object = new NotesApplication();
+		noteapp_object.create_notes(note);
+		var hold_notes = noteapp_object.list_notes();
+		assert.deepEqual([note], hold_notes);
+	})
+})
+
+describe("Check that a user can search for text in notes", function(){
+	it("should check that a user can search for specific text", function(){
+		var note = new Notes({author: "kachi", note_content: "stuff"});
+		var note1 = new Notes({author: "me", note_content: "more stuff"});
+		var noteapp_object = new NotesApplication();
+		noteapp_object.create_notes(note);
+		noteapp_object.create_notes(note1);
+		noteapp_object.search("s");
+		assert(["stuff", "more stuff"]);
 	})
 })
